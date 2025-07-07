84afe075cf99ee1c74a37e02dcad3620d1324ca4
Hayssa Fonseca <hayssa.fonseca40@gmail.com>
2025-07-07 13:07:40 -0300

    Ex_3_aula.cpp

--- Ex_3_aula.cpp
+++ Ex_3_aula.cpp
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main (){
-	int num;
+	int numero;
 	printf("Digite um numero inteiro: ");
-	scanf("%d", &num);
+	scanf("%d", &numero);
 	
-	if(num % 2 == 0){
-		printf("o numero %d eh par \n", num);
+	if(numero % 2 == 0){
+		printf("o numero %d eh par \n", numero);
 	}else{
-		printf("o numero %d eh impar \n", num);
+		printf("o numero %d eh impar \n", numero);
 	}
 	
 	getchar();
