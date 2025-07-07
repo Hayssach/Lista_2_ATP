e5787eb0474f3a7ae4d7795e1b4f44b6f216de19
Hayssa Fonseca <hayssa.fonseca40@gmail.com>
2025-07-07 13:08:51 -0300

    Ex_4_aula.cpp

--- Ex_4_aula.cpp
+++ Ex_4_aula.cpp
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 int main (){
-	int idade, anos_servicos;
+	int i, anos_s;
 	printf("Digite sua idade: ");
-	scanf("%d", &idade);
+	scanf("%d", &i);
 	printf("Digite os anos de servicos: ");
-	scanf("%d", &anos_servicos);
+	scanf("%d", &anos_s);
 	
-	if(idade >= 65 || anos_servicos >= 30 || (idade >= 60 && anos_servicos >= 25)){
+	if(i >= 65 || anos_s >= 30 || (i >= 60 && anos_se >= 25)){
 		printf("Voce pode se aposentar.\n");
 	}else{
 		printf("Voce nao pode se aposentar.\n");
