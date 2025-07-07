4f884ba34c28f5a2c60bbec0f72e76bc2f248e99
Hayssa Fonseca <hayssa.fonseca40@gmail.com>
2025-07-07 13:06:15 -0300

    Ex_2_aula.cpp

--- Ex_2_aula.cpp
+++ Ex_2_aula.cpp
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 int main(){
-	int num, valor_absoluto;
+	int num, valor_abs;
 	
 	printf("Digite um numero: ");
 	scanf("%d", &num);
 	
 	if(num < 0){
-		valor_absoluto = -num;
+		valor_abs = -num;
 		
 	}else{
-		valor_absoluto = num;
+		valor_abs = num;
 	}
 	
-	printf("O valor absoluto de %d eh %d\n: ", num, valor_absoluto);
+	printf("O valor absoluto de %d eh %d\n: ", num, valor_abs);
 	
 	getchar();
 	return(0);
